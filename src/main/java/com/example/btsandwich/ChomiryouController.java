fdc5368ef2c846e05c7deaca7dfd53b13621788d
FieBee <phuonglf98@gmail.com>
2022-09-10 10:10:17 +0700

    hoanthanh

---
+++ src/main/java/com/example/btsandwich/ChomiryouController.java
@@ -0,0 +1,27 @@
+package com.example.btsandwich;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+
+@Controller
+public class ChomiryouController {
+
+    @GetMapping("")
+    public String home(){
+        return "index";
+    }
+
+    @PostMapping("/result")
+    public ModelAndView save(@RequestParam ArrayList<String> condiment){
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("result",condiment);
+        return modelAndView;
+    }
+}
